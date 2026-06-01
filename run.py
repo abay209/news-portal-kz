81f16c8b5902e5d0e6c66f34136581e3eecc490a
Пользователь <user@example.com>
2026-06-01 06:26:11 +0500

    revert: move parser back into run.py to share Volume with web server

--- run.py
+++ run.py
@@ -1,5 +1,20 @@
 from app import create_app
+from rss_parser import fetch_rss_feeds
+from apscheduler.schedulers.background import BackgroundScheduler
+import atexit
+
 app = create_app()
+
+# Initialize Scheduler
+scheduler = BackgroundScheduler()
+# Запуск парсера сразу при старте
+scheduler.add_job(func=fetch_rss_feeds, trigger="date")
+# Затем каждые 30 минут
+scheduler.add_job(func=fetch_rss_feeds, trigger="interval", minutes=30)
+scheduler.start()
+
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
 
 if __name__ == '__main__':
     # В Windows для продакшена лучше использовать waitress (указано в requirements.txt)
